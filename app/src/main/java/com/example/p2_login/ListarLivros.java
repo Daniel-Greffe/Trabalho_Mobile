030de874af58cec0107329bf868b39de1456cb14
Erik Gaborim <erikgfp@gmail.com>
2022-06-07 17:29:37 -0400

    Criei a classe livro, fiz o design da tela de cadastro e comecei a tela de listar livros

---
+++ app/src/main/java/com/example/p2_login/ListarLivros.java
@@ -0,0 +1,14 @@
+package com.example.p2_login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ListarLivros extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_listar_livros);
+    }
+}
