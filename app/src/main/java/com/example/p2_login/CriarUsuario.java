d77ffb7ae38b4bf326e780e8950abeae710cee64
Erik Gaborim <erikgfp@gmail.com>
2022-06-06 20:25:42 -0400

    Iniciando a conexão com o banco para a validação do login e criação de usuário

--- app/src/main/java/com/example/p2_login/CriarUsuario.java
+++ app/src/main/java/com/example/p2_login/CriarUsuario.java
@@ -1,15 +1,26 @@
 package com.example.p2_login;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class CriarUsuario extends AppCompatActivity {
 
     EditText cadastrarEmail, cadastrarSenha;
     Button btCadastrar;
+    FirebaseAuth mAuthCria;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +28,41 @@ public class CriarUsuario extends AppCompatActivity {
         setContentView(R.layout.activity_criar_usuario);
 
         cadastrarEmail  = findViewById(R.id.loginEmail);
+
+        mAuthCria = FirebaseAuth.getInstance();
+
+        btCadastrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                criarUSuario(cadastrarEmail.getText().toString(), cadastrarSenha.getText().toString());
+            }
+        });
+    }
+
+    private void criarUSuario(String email, String senha) {
+        if(email.equals("") || !Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            cadastrarEmail.setError("Preencha corretamente");
+            cadastrarEmail.requestFocus();
+            return;
+        }
+
+        if(senha.equals("")){
+            cadastrarSenha.setError("Preencha corretamente");
+            cadastrarSenha.requestFocus();
+            return;
+        }
+
+        mAuthCria.createUserWithEmailAndPassword(email, senha).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()){
+                    Toast.makeText(getApplicationContext(), "Usuário criado com sucesso", Toast.LENGTH_LONG).show();
+                    Intent intent = new Intent(CriarUsuario.this, MainActivity.class);
+                    startActivity(intent);
+                }
+                else
+                    Toast.makeText(getApplicationContext(), "Erro ao criar usuário", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
--- app/src/main/java/com/example/p2_login/MainActivity.java
+++ app/src/main/java/com/example/p2_login/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.p2_login;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -11,30 +13,33 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    EditText email, senha;
+    EditText inputEmail, inputSenha;
     TextView recSenha, criarUsuario;
     Button btLogar;
     ProgressBar progressBar;
 
     FirebaseAuth mAuth;
-
-    Intent i;
+    Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        email  = findViewById(R.id.loginEmail);
-        senha  = findViewById(R.id.loginSenha);
+        inputEmail  = findViewById(R.id.loginEmail);
+        inputSenha  = findViewById(R.id.loginSenha);
         criarUsuario  = findViewById(R.id.textViewCriaUsuario);
         recSenha  = findViewById(R.id.textViewEsqueciSenha);
         btLogar  = findViewById(R.id.btCadastrar);
-//        progressBar  = findViewById(R.id.progressBar);
+        //progressBar  = findViewById(R.id.progressBar);
 
         criarUsuario.setOnClickListener(this);
 
@@ -45,16 +50,55 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View view) {
         switch (view.getId()){
             case R.id.btCadastrar:
-                //logar();
+                logar();
                 break;
             case R.id.textViewEsqueciSenha:
-                //i = new Intent(MainActivity.this, RecuperaSenha.class);
-                startActivity(i);
+                //intent = new Intent(MainActivity.this, RecuperaSenha.class);
+                startActivity(intent);
                 break;
             case R.id.textViewCriaUsuario:
-                i = new Intent(MainActivity.this, CriarUsuario.class);
-                startActivity(i);
+                intent = new Intent(MainActivity.this, CriarUsuario.class);
+                startActivity(intent);
                 break;
         }
+    }
+
+    private void logar() {
+        String email = inputEmail.getText().toString();
+        String senha = inputSenha.getText().toString();
+
+        if(email.equals("") || !Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            inputEmail.setError("Preencha corretamente");
+            inputSenha.requestFocus();
+            return;
+        }
+
+        if(senha.equals("")){
+            inputSenha.setError("Preencha corretamente");
+            inputSenha.requestFocus();
+            return;
+        }
+
+        progressBar.setVisibility(View.VISIBLE);
+        mAuth.signInWithEmailAndPassword(email, senha).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                    if(user.isEmailVerified()){
+                        intent = new Intent(MainActivity.this, UsuarioLogado.class);
+                        startActivity(intent);
+                    }
+                    else {
+                        Toast.makeText(MainActivity.this, "Confirme seu login por email", Toast.LENGTH_LONG).show();
+                        user.sendEmailVerification();
+                    }
+                }
+                else {
+                    Toast.makeText(MainActivity.this, "Erro ao logar", Toast.LENGTH_LONG).show();
+                }
+                progressBar.setVisibility(View.GONE);
+            }
+        });
     }
 }
